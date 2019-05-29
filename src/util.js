65d44e734737c0f7b4417a013933b68e26d96064
Pobepto <roobertdantes@gmail.com>
2019-05-29 10:54:24 +0300

    feat: test reconnectiong feature

--- src/util.js
+++ src/util.js
@@ -2,37 +2,40 @@ import txHelper from './txHelper'
 import { TxStatus, TxStatusRequest } from './proto/endpoint_pb'
 
 import { from, Observable } from 'rxjs'
-import { map, concatMap, toArray } from 'rxjs/operators'
+import { map, retryWhen, delay, exhaustMap, first } from 'rxjs/operators'
 
 function _listToTorii (txs, txClient, timeoutLimit) {
   const txList = txHelper.createTxListFromArray(txs)
+  return new Promise((resolve, reject) => {
+    /**
+     * grpc-node hangs against unresponsive server, which possibly occur when
+     * invalid node IP is set. To avoid this problem, we use timeout timer.
+     * c.f. {@link https://github.com/grpc/grpc/issues/13163 Grpc issue 13163}
+     */
 
-  return from(
-    new Promise((resolve, reject) => {
-      /**
-       * grpc-node hangs against unresponsive server, which possibly occur when
-       * invalid node IP is set. To avoid this problem, we use timeout timer.
-       * c.f. {@link https://github.com/grpc/grpc/issues/13163 Grpc issue 13163}
-       */
+    const timer = setTimeout(() => {
+      txClient.$channel.close()
+      reject(new Error('Please check IP address OR your internet connection'))
+    }, timeoutLimit)
 
-      const timer = setTimeout(() => {
-        txClient.$channel.close()
-        reject(new Error('Please check IP address OR your internet connection'))
-      }, timeoutLimit)
+    // Sending even 1 transaction to listTorii is absolutely ok and valid.
+    txClient.listTorii(txList, (err, data) => {
+      clearTimeout(timer)
 
-      // Sending even 1 transaction to listTorii is absolutely ok and valid.
-      txClient.listTorii(txList, (err, data) => {
-        clearTimeout(timer)
+      if (err) {
+        return reject(err)
+      }
 
-        if (err) {
-          return reject(err)
-        }
-
-        const hashes = txs.map(x => txHelper.hash(x))
-        resolve(hashes)
-      })
+      const hashes = txs.map(x => txHelper.hash(x))
+      resolve(hashes)
     })
-  )
+  })
+}
+
+function _handleStream (hash, txClient) {
+  const request = new TxStatusRequest()
+  request.setTxHash(hash.toString('hex'))
+  return txClient.statusStream(request)
 }
 
 function fromStream (stream) {
@@ -83,31 +86,46 @@ function sendTransactions (txs, txClient, timeoutLimit, requiredStatuses = [
   'MST_PENDING',
   'COMMITTED'
 ]) {
+  const terminalStatuses = [
+    TxStatus.STATELESS_VALIDATION_FAILED,
+    TxStatus.STATEFUL_VALIDATION_FAILED,
+    TxStatus.COMMITTED,
+    TxStatus.MST_EXPIRED,
+    TxStatus.NOT_RECEIVED,
+    TxStatus.REJECTED,
+    TxStatus.UNRECOGNIZED
+  ]
+
   return _listToTorii(txs, txClient, timeoutLimit)
-    .pipe(
-      concatMap(hashes => {
-        const request = new TxStatusRequest()
-        request.setTxHash(hashes[0].toString('hex'))
-        return fromStream(
-          txClient.statusStream(request)
+    .then(hashes => {
+      return from(hashes)
+        .pipe(
+          exhaustMap(hash => fromStream(
+            _handleStream(hash, txClient))
+          ),
+          map(tx => {
+            if (!terminalStatuses.includes(tx.getTxStatus())) {
+              throw tx
+            }
+            return tx
+          }),
+          retryWhen(err => err.pipe(
+            delay(1000)
+          )),
+          first(tx => terminalStatuses.includes(
+            tx.getTxStatus()
+          ))
         )
-      }),
-      map(tx => getProtoEnumName(
-        TxStatus,
-        'iroha.protocol.TxStatus',
-        tx.getTxStatus()
-      )),
-      toArray()
-    )
-    .toPromise()
-    .then(statuses => {
-      const statusCheck = statuses.some(s => requiredStatuses.includes(s))
-      if (statusCheck) {
-        return Promise.resolve()
+        .toPromise()
+    })
+    .then(tx => {
+      const status = getProtoEnumName(TxStatus, 'iroha.protocol.TxStatus', tx.getTxStatus())
+      if (requiredStatuses.includes(status)) {
+        return Promise.resolve(tx.toObject())
       }
 
       return Promise.reject(
-        new Error(`Command response error: expected=${requiredStatuses}, actual=${statuses}`)
+        new Error(`Command response error: expected=${requiredStatuses}, actual=${status}`)
       )
     })
 }
