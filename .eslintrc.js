b231aad9fa4dc57392605ba46a1e72dbddbb4580
Pobepto <roobertdantes@gmail.com>
2019-06-20 15:48:15 +0300

    feat: update configs for ts

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,14 @@
 module.exports = {
-    "extends": "standard"
-};
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    project: './tsconfig.json'
+  },
+  extends: [
+    'plugin:@typescript-eslint/recommended'
+  ],
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module'
+  },
+  rules: {}
+}
